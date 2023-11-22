0985ba4e9c9b54c285061b1a25013f6019cd0dcb
SurryChen <1974023122@qq.com>
2023-11-22 17:34:38 +0800

    [test] first test

---
+++ src/main/java/com/somecode/automatedeploymenttest/controller/AutoController.java
@@ -0,0 +1,21 @@
+package com.somecode.automatedeploymenttest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/auto")
+public class AutoController {
+
+    @GetMapping("/deploy")
+    public String deploy() {
+        return "Success!";
+    }
+
+    @GetMapping("/deploys")
+    public String deploys() {
+        return "You are success!";
+    }
+
+}
---
+++ src/test/java/com/somecode/automatedeploymenttest/AutomateDeploymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somecode.automatedeploymenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomateDeploymentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
